9a77bb26838463763b8ac5a0abfd843c7ca0c18f
adarsh <adarsh2858@gmail.com>
2020-12-09 00:00:26 +0530

    refine folder structure and file names

--- server/index.js
+++ server/index.js
@@ -1,31 +1,25 @@
-// var x = document.getElementById("view_campgrounds");
-
-// x.addEventListener("click", () =>{
-//     window.location="html/campgrounds.html"
-// });
-
 const express = require("express");
 const app = express();
 const port = 3000;
 
-const axios = require('axios');
+const axios = require("axios");
 const { response } = require("express");
 
+app.use(express.static('public'));
+
+
 app.get("/", (req, res) => {
-//   res.send("<h1>Hello World</h1>");
-  res.sendFile('index.html', { root: "public" })
+  res.sendFile("index.html", { root: "public" });
 });
 
 app.get("/campgrounds", (req, res) => {
-      res.sendFile('campgrounds.html', { root: "public" })
-    });
+  res.sendFile("campgrounds.html", { root: "public" });
+});
 
-app.get("/users",(req, res) => {
-  axios.get('https://randomuser.me/api/?page=1&results=10').then(
-    response => {
-      res.send(response.data);
-    }
-  );
+app.get("/users", (req, res) => {
+  axios.get("https://randomuser.me/api/?page=1&results=10").then((response) => {
+    res.send(response.data);
+  });
 });
 
 app.listen(port, () => {

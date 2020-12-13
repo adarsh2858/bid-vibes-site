03224226d60e75918ef1f9127fb72bba92c89f21
adarsh <adarsh2858@gmail.com>
2020-12-14 01:52:11 +0530

    style the new product page with bootstrap

--- client/src/components/NewProduct.js
+++ client/src/components/NewProduct.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 
 const NewProduct = () => {
   return (
-    <div>
+    <div className="p-4">
       <form method="post" action="/products/new">
-        <label>Name</label>
-        <input type="text" name="name" />
-        <label>Description</label>
-        <textarea rows="10" name="description" />
-        <button>Submit</button>
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" name="name" />
+        </div>
+          <label>Description</label>
+        <div className="form-group">
+          <textarea rows="10" name="description" />
+        </div>
+        <button className="btn btn-success">Submit</button>
       </form>
     </div>
   );

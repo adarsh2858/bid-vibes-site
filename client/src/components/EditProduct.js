336b99a2fc883afbc15aa999371e3407dddb97b3
adarsh <adarsh2858@gmail.com>
2021-01-31 15:35:17 +0530

    load product with useEffect and useState for the react component

--- client/src/components/EditProduct.js
+++ client/src/components/EditProduct.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const EditProduct = (props) => {
+const EditProduct = () => {
+  const [loadedProduct, setLoadedProduct] = useState(null);
 
-  console.log(props);
+  useEffect(() => {
+    const loadProduct = async () => {
+      const productInfo = await fetch(window.location.href + "Info");
+      const product = await productInfo.json();
+      setLoadedProduct(product);
+    };
+    loadProduct();
+  }, []);
 
   return (
     <div className="p-4">
-      <form action="/products/:id/edit" method="PUT">
-        <div className="form-group">
-          <label>Name</label>
-          <input type="text" name="name" value={props.name} />
-        </div>
+      {loadedProduct && (
+        <form action="edit" method="POST">
+          <div className="form-group">
+            <label>Name</label>
+            <input type="text" name="name" defaultValue={loadedProduct.name} />
+          </div>
           <label>Description</label>
-        <div className="form-group">
-          <textarea rows="10" name="description" value={props.description} />
-        </div>
-        <button className="btn btn-success">Submit</button>
-      </form>
+          <div className="form-group">
+            <textarea
+              rows="10"
+              name="description"
+              defaultValue={loadedProduct.description}
+            />
+          </div>
+          <button className="btn btn-success">Submit</button>
+        </form>
+      )}
     </div>
   );
 };

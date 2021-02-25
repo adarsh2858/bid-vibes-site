1a03d6b41c9b2942bdf95c859c0627f6125de846
adarsh2858 <adarsh2858@gmail.com>
2021-02-26 00:09:53 +0530

    add a basic formik form to accept public comments

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,8 +1,44 @@
 import React from "react";
+import { Formik } from "formik";
 
 export default class Comments extends React.Component {
-    render() {
-        return <div>Hello World!!</div>
-    }
-}
+  //   handleSubmit = () => {
+  //     console.log("Submitting comment form");
+  //   };
 
+  render() {
+    return (
+      <div>
+        <Formik
+          initialValues={{ username: "", comment: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log(JSON.stringify(values));
+            setSubmitting(false);
+          }}
+        >
+          {({ handleChange, isSubmitting, handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <input
+                className="form-control m-2"
+                placeholder="Enter your username"
+                name="username"
+                type="text"
+                onChange={handleChange}
+              />
+              <input
+                className="form-control m-2"
+                placeholder="Enter your comment"
+                name="comment"
+                type="text"
+                onChange={handleChange}
+              />
+              <button className="btn btn-success" type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
+}

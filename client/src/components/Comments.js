993125ca7f587368774d00f2a32c06dccc241190
adarsh2858 <adarsh2858@gmail.com>
2021-02-28 13:21:54 +0530

    reset the form after submission and define a new function to handle the comment form submission

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -2,27 +2,27 @@ import React from "react";
 import { Formik } from "formik";
 
 export default class Comments extends React.Component {
-  //   handleSubmit = () => {
-  //     console.log("Submitting comment form");
-  //   };
+  async handleFormSubmission(values, { setSubmitting, resetForm }) {
+    console.log(JSON.stringify(values));
+    setSubmitting(false);
+    resetForm({});
+  }
 
   render() {
     return (
       <div>
         <Formik
           initialValues={{ username: "", comment: "" }}
-          onSubmit={(values, { setSubmitting }) => {
-            console.log(JSON.stringify(values));
-            setSubmitting(false);
-          }}
+          onSubmit={this.handleFormSubmission}
         >
-          {({ handleChange, isSubmitting, handleSubmit }) => (
+          {({ values, handleChange, isSubmitting, handleSubmit }) => (
             <form onSubmit={handleSubmit}>
               <input
                 className="form-control m-2"
                 placeholder="Enter your username"
                 name="username"
                 type="text"
+                value={values.username || ""}
                 onChange={handleChange}
               />
               <input
@@ -30,9 +30,14 @@ export default class Comments extends React.Component {
                 placeholder="Enter your comment"
                 name="comment"
                 type="text"
+                value={values.comment || ""}
                 onChange={handleChange}
               />
-              <button className="btn btn-success" type="submit" disabled={isSubmitting}>
+              <button
+                className="btn btn-success"
+                type="submit"
+                disabled={isSubmitting}
+              >
                 Submit
               </button>
             </form>

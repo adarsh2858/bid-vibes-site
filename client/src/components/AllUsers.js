8b6d0e228749f54aa88c5f867c04a22935f0cf55
adarsh2858 <adarsh2858@gmail.com>
2021-03-21 23:30:03 +0530

    validate DOM nesting for the table tag and add inline styles

--- client/src/components/AllUsers.js
+++ client/src/components/AllUsers.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const AllUsers = () => {
   const [users, setUsers] = useState([]);
@@ -14,29 +14,44 @@ const AllUsers = () => {
     setUsers(usersList.data);
   };
 
+  useEffect(() => fetchUsers(), []);
+
   return (
     <div className="p-3">
       <h1>ALL USERS</h1>
-      <button onClick={fetchUsers}>Fetch Users</button>
-      {users ? (
-        <div className="">
-          <table className="table-headers">
+      <button onClick={fetchUsers}>Fetch Latest Users</button>
+      {users.length > 0 ? (
+        <table
+          className="m-4"
+          id="users-table"
+          style={{ width: "75%", height: screen.height }}
+        >
+          <thead>
             <tr>
               <th>First Name</th>
               <th>Last Name</th>
               <th>Age</th>
             </tr>
+          </thead>
+          <tbody>
             {users.map(({ id, first, last, age }) => {
               return (
-                <tr className="bg-light m-2 p-2" key={id}>
+                <tr key={id}>
                   <td className="text-capitalize">{first}</td>
                   <td>{last}</td>
                   <td>{age}</td>
                 </tr>
               );
             })}
-          </table>
-        </div>
+          </tbody>
+          {/* <tfoot>
+            <tr>
+              <td>Total Entries</td>
+              <td>1</td>
+              <td>2</td>
+            </tr>
+          </tfoot> */}
+        </table>
       ) : null}
     </div>
   );

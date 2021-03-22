e1615a5409d0591400943da95699716b2f1ae93b
adarsh2858 <adarsh2858@gmail.com>
2021-03-23 00:55:31 +0530

    have pagination for the 2 pages with 5 users each

--- client/src/components/AllUsers.js
+++ client/src/components/AllUsers.js
@@ -1,15 +1,25 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import classnames from "classnames";
 import "../../public/css/all-users.css";
 
 const AllUsers = () => {
   const [users, setUsers] = useState([]);
+  const [showUsers, setShowUsers] = useState([]);
   const [hoverEffect, setHoverEffect] = useState(false);
+
+  const firstRef = useRef(null);
+  const firstRender = useRef(true);
+
+  const [lengthValue, setLengthValue] = useState(5);
+  const [startValue, setStartValue] = useState(0);
 
   const paginationButtonsClassnames = classnames(
     "p-2 m-2 text-decoration-none",
-    { "bg-primary": hoverEffect, "text-white": hoverEffect }
+    {
+      // "bg-primary": hoverEffect,
+      "text-white": hoverEffect,
+    }
   );
 
   const fetchUsers = async () => {
@@ -20,17 +30,32 @@ const AllUsers = () => {
     });
 
     setUsers(usersList.data);
+    setShowUsers(usersList.data.slice(0, 5));
+  };
+
+  const fetchTableData = () => {
+    let showUsers = [];
+    for (let i = startValue; i < lengthValue; i++) {
+      showUsers.push(users[i]);
+    }
+    setShowUsers(showUsers);
   };
 
   useEffect(() => {
+    console.log(firstRender);
+    if (!firstRender.current) fetchTableData();
+  }, [startValue, lengthValue]);
+
+  useEffect(() => {
     fetchUsers();
+    firstRender.current = false;
   }, []);
 
   return (
     <div className="p-3">
       <h1>ALL USERS</h1>
       <button onClick={fetchUsers}>Fetch Latest Users</button>
-      {users.length > 0 ? (
+      {showUsers.length > 0 ? (
         <div>
           <table
             className="m-4"
@@ -45,7 +70,7 @@ const AllUsers = () => {
               </tr>
             </thead>
             <tbody>
-              {users.map(({ id, first, last, age }) => {
+              {showUsers.map(({ id, first, last, age }) => {
                 return (
                   <tr key={id}>
                     <td className="text-capitalize">{first}</td>
@@ -57,20 +82,37 @@ const AllUsers = () => {
             </tbody>
           </table>
           <div className="d-flex">
-            <a href="#" className={paginationButtonsClassnames}>
-              &laquo;
-            </a>
-            <a href="#" className={paginationButtonsClassnames} style={{cursor: "pointer"}}>
-              1
-            </a>
             <a
               href="#"
               onMouseEnter={() => setHoverEffect(true)}
               onMouseLeave={() => setHoverEffect(false)}
               className={paginationButtonsClassnames}
             >
-              2
+              &laquo;
             </a>
+            <button
+              ref={firstRef}
+              onMouseEnter={() => setHoverEffect(true)}
+              onMouseLeave={() => setHoverEffect(false)}
+              className={paginationButtonsClassnames}
+              onClick={() => {
+                setStartValue(0);
+                setLengthValue(5);
+              }}
+            >
+              1
+            </button>
+            <button
+              onMouseEnter={() => setHoverEffect(true)}
+              onMouseLeave={() => setHoverEffect(false)}
+              className={paginationButtonsClassnames}
+              onClick={() => {
+                setStartValue(5);
+                setLengthValue(10);
+              }}
+            >
+              2
+            </button>
             <a
               href="#"
               onMouseEnter={() => setHoverEffect(true)}
@@ -79,10 +121,12 @@ const AllUsers = () => {
             >
               3
             </a>
-            <a href="#" className={paginationButtonsClassnames}>
-              4
-            </a>
-            <a href="#" className={paginationButtonsClassnames}>
+            <a
+              href="#"
+              onMouseEnter={() => setHoverEffect(true)}
+              onMouseLeave={() => setHoverEffect(false)}
+              className={paginationButtonsClassnames}
+            >
               &raquo;
             </a>
           </div>

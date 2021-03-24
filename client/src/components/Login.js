5d3f18330b70481fe5c5d818f1db1c4011349032
adarsh2858 <adarsh2858@gmail.com>
2021-03-24 17:39:28 +0530

    refactor code in the login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,38 +4,36 @@ import { Formik, Form, Field } from "formik";
 import axios from "axios";
 
 const Login = () => {
-  const firstVariable = "Hello World";
+  const submitLoginForm = async (values) => {
+    // await new Promise((r) => setTimeout(r, 2000));
+    try {
+      const response = await axios({
+        method: "post",
+        url: "/login",
+        data: values,
+      });
 
-  const submitLoginForm = () => {};
+      toast.success(response.data.message);
+
+      setTimeout(() => {
+        window.location = response.data.redirectTo;
+      }, 2000);
+    } catch (err) {
+      toast.error(err.response.data.message);
+    }
+  };
 
   return (
     <div>
       <ToastContainer />
-      <h1 className="text-center">Sign in</h1>
+      <h1 className="text-center pt-4">Sign in</h1>
 
       <Formik
         initialValues={{
           username: "",
           password: "",
         }}
-        onSubmit={async (values) => {
-          // await new Promise((r) => setTimeout(r, 2000));
-          try {
-            const response = await axios({
-              method: "post",
-              url: "/login",
-              data: values,
-            });
-
-            toast.success(response.data.message);
-
-            setTimeout(() => {
-              window.location = response.data.redirectTo;
-            }, 2000);
-          } catch (err) {
-            toast.error(err.response.data.message);
-          }
-        }}
+        onSubmit={submitLoginForm}
       >
         {({ isSubmitting }) => (
           <Form>
@@ -48,7 +46,7 @@ const Login = () => {
                 placeholder="Enter username"
                 type="text"
                 required
-              ></Field>
+              />
             </div>
 
             <div className="form-group">
@@ -59,7 +57,7 @@ const Login = () => {
                 placeholder="Enter password"
                 type="password"
                 required
-              ></Field>
+              />
             </div>
 
             <button
@@ -72,33 +70,6 @@ const Login = () => {
           </Form>
         )}
       </Formik>
-
-      <form action="/login" method="POST">
-        <h1 className="text-center">Sign in</h1>
-        <div className="form-group">
-          <label>Username</label>
-          <input
-            className="form-control"
-            placeholder="Enter username"
-            type="text"
-            name="username"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            className="form-control"
-            placeholder="Enter password"
-            type="password"
-            name="password"
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
     </div>
   );
 };

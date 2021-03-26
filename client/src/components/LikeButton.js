dceb7defc0030e1901614a0ba149f987fd24bf82
adarsh2858 <adarsh2858@gmail.com>
2021-03-26 11:25:48 +0530

    fix the like button on the landing page

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -10,13 +10,13 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
+      return "You liked this.";
     }
 
     return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+      "button",
+      { className: "btn btn-primary", onClick: () => this.setState({ liked: true }) },
+      "Like"
     );
   }
 }

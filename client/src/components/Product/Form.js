9da5102b9f692bfd650b6620cc0163caa40d8290
adarsh2858 <adarsh2858@gmail.com>
2021-03-29 11:30:43 +0530

    add a loading spinner while the image is getting uploaded

---
+++ client/src/components/Product/Form.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useRef, useState } from "react";
+import Spinner from "react-bootstrap/spinner";
+
+const ProductForm = (props) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadedImage, setUploadedImage] = useState({ secure_url: "" });
+  const [loadedProduct, setLoadedProduct] = useState(null);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(false);
+  const submitButtonRef = useRef(null);
+
+  useEffect(() => {
+    const loadProduct = async () => {
+      const productInfo = await fetch(window.location.href + "Info");
+      const product = await productInfo.json();
+      setLoadedProduct(product);
+
+      const image = { secure_url: product.image || "" };
+      setUploadedImage(image);
+    };
+    loadProduct();
+  }, []);
+
+  const onFileChange = (event) => {
+    setIsButtonDisabled(true);
+    setShowSpinner(true);
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const onFileUpload = async () => {
+    const formData = new FormData();
+    formData.append("myFile", selectedFile);
+
+    const imageInfo = await fetch("http://localhost:3000/image-upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const image = await imageInfo.json();
+    setUploadedImage(image);
+    setIsButtonDisabled(false);
+    setShowSpinner(false);
+  };
+
+  useEffect(() => {
+    if (selectedFile) onFileUpload();
+  }, [selectedFile]);
+
+  const handleChange = (event) => {
+    setUploadedImage({ secure_url: event.target.value });
+  };
+
+  useEffect(() => {
+    if (submitButtonRef.current) {
+      submitButtonRef.current.classList.add("btn");
+      submitButtonRef.current.classList.add("btn-success");
+    }
+  }, [submitButtonRef.current]);
+
+  return (
+    <div className="p-4 relative">
+      {loadedProduct && (
+        <form action={props.action} method="POST">
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              defaultValue={loadedProduct.name}
+            />
+          </div>
+          <label>Description</label>
+          <div className="form-group">
+            <textarea
+              className="form-control"
+              rows="10"
+              name="description"
+              defaultValue={loadedProduct.description}
+            />
+          </div>
+          <div className="form-group">
+            <input type="file" onChange={onFileChange} />
+
+            {showSpinner ? (
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Spinner animation="border" variant="success" />
+              </div>
+            ) : null}
+
+            <input
+              name="image"
+              type="text"
+              value={uploadedImage.secure_url}
+              onChange={handleChange}
+              placeholder="Image URL"
+            />
+            {uploadedImage.secure_url != "" && (
+              <img
+                width="300"
+                className="img-fluid block m-4"
+                src={uploadedImage.secure_url}
+                alt="Product Image"
+              />
+            )}
+          </div>
+          <button ref={submitButtonRef} disabled={isButtonDisabled}>
+            Submit
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default ProductForm;
